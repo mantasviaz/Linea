669b3c2e7a765b80178623db725214cda842d226
Junie Guo <151979734+junieguo@users.noreply.github.com>
2025-04-23 17:56:12 -0400

    Initial Project Creation

---
+++ Linea/LineaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineaApp.swift
+//  Linea
+//
+//  Created by Junie Guo on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct LineaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
