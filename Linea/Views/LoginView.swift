5c74c94f3120c4da9ab1073a9dc82a68b85ed1a0
Mantas Viazmitinas <mantasv@seas.upenn.edu>
2025-04-23 18:24:36 -0400

    Added basic view template

--- Linea/Views/LoginView.swift
+++ Linea/Views/LoginView.swift
@@ -3,3 +3,14 @@
 //  Linea
 //
 
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
