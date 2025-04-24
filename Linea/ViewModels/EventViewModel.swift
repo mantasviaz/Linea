5648a47a60a3a1432a27e13e6ac159ae3ca37d2a
Mantas Viazmitinas <mantasv@seas.upenn.edu>
2025-04-24 12:52:59 -0400

    renamed to fit naming conventions

--- Linea/ViewModels/EventViewModel.swift
+++ Linea/ViewModels/EventViewModel.swift
@@ -6,6 +6,7 @@
 import Foundation
 import SwiftUI
 
+@Observable
 class TaskViewModel {
     var tasks: [Task] = []
 }
--- Linea/Views/TaskBar.swift
+++ Linea/Views/TaskBar.swift
@@ -1,5 +1,5 @@
 //
-//  TaskView.swift
+//  TaskBar.swift
 //  Linea
 //
 //  
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct TaskView: View {
+struct TaskBar: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    TaskView()
+    TaskBar()
 }
